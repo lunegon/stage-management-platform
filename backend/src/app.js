805ee0d48028314d95e854dd1b795b407d32cc22
lunegon <lucas.goncalves78@outlook.fr>
2026-03-10 16:06:58 +0000

    Connect Offer routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const offerRoutes = require("./routes/offerRoutes");
